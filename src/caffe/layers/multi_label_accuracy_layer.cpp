ed64c6a18e6ac8fe811b9d345afede9983b19e3e
adm <adm@idea.pc>
2015-12-27 16:33:49 +0800

    'zz'

--- src/caffe/layers/multi_label_accuracy_layer.cpp
+++ src/caffe/layers/multi_label_accuracy_layer.cpp
@@ -6,7 +6,7 @@
 #include <vector>
 
 #include "caffe/layer.hpp"
-#include "caffe/vision_layers.hpp"
+//#include "caffe/vision_layers.hpp"
 #include "caffe/util/math_functions.hpp"
 #include "caffe/util/io.hpp"
 
