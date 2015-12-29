686b6bcd50f6dc62f701839590677c57789ba4b8
adm <adm@idea.pc>
2015-12-29 13:40:05 +0800

    'zz'

--- src/caffe/layers/sigmoid_cross_entropy_loss_layer.cpp
+++ src/caffe/layers/sigmoid_cross_entropy_loss_layer.cpp
@@ -14,15 +14,16 @@ void SigmoidCrossEntropyLossLayer<Dtype>::LayerSetUp(
   sigmoid_top_vec_.clear();
   sigmoid_top_vec_.push_back(sigmoid_output_.get());
   sigmoid_layer_->SetUp(sigmoid_bottom_vec_, sigmoid_top_vec_);
-  if (top.size() >= 1) {
-    // sigmoid cross entropy loss (averaged across batch)
-    top[0]->Reshape(1, 1, 1, 1);
-  }
-  if (top.size() == 2) {
-    // softmax output
-    top[1]->ReshapeLike(sigmoid_output_.get());
-    top[1]->ShareData(sigmoid_output_.get());
-  }
+  
+  //if (top.size() >= 1) {
+  //  // sigmoid cross entropy loss (averaged across batch)
+  //  top[0]->Reshape(1, 1, 1, 1);
+  //}
+  //if (top.size() == 2) {
+  //  // softmax output
+  //  top[1]->ReshapeLike(sigmoid_output_.get());
+  //  top[1]->ShareData(sigmoid_output_.get());
+  //}
 }
 
 template <typename Dtype>
